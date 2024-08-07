6f70f59f774d55cd146b2a25b11059d8b2961b44
yang <zy892065502@gmail.com>
2024-08-07 11:24:02 +0800

    fix redirect bug

--- middleware.js
+++ middleware.js
@@ -1,17 +1,20 @@
 import { locales } from './lib/i18n';
-import { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-	const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-	const isExit = locales.some((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`);
+  // 检查是否已经在根路径
+  if (pathname === '/') return;
 
-	if (isExit) return;
+  const isExist = locales.some((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`);
 
-	request.nextUrl.pathname = `/`;
-	return Response.redirect(request.nextUrl);
+  if (isExist) return;
+
+  // 只有当路径不是根路径且不包含 locale 时才重定向
+  return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
-	matcher: ['/((?!_next)(?!.*\\.(?:ico|png|gif|svg|jpg|jpeg|xml|txt|mp4)$)(?!/api).*)'],
+  matcher: ['/((?!_next)(?!.*\\.(?:ico|png|gif|svg|jpg|jpeg|xml|txt|mp4)$)(?!/api).*)'],
 };
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'landingpage.huglemon.com',
+				hostname: 'bookmarkorg.com',
 			},
 		],
 	},
